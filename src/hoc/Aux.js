1703b2fbb23fc63cf6a9dc995764aea1f8d66ae0
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-04-24 22:12:21 -0400

    Added basic structure and hoc wrapper

---
+++ src/hoc/Aux.js
@@ -0,0 +1,7 @@
+// Higher Order Function created to wrap multiple elements 
+// replaces need for using a wrapping div to negate inability to return
+// multiple elements next to one another
+
+const aux = (props) => props.children;
+
+export default aux;
