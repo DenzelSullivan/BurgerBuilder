112a79ed716c36b20d07d402045b94ff11e78066
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-03 15:25:47 -0400

    Ejecting axios interceptors

--- src/containers/hoc/withErrorHandler/withErrorHandler.js
+++ src/containers/hoc/withErrorHandler/withErrorHandler.js
@@ -6,17 +6,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         constructor(){
             super()
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 console.log(error);
                 this.setState({error: error});
             })
         }
         state = {
             error: null
+        }
+
+        componentWillUnmount(){
+            console.log('WIll Unmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
