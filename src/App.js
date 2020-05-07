0af16c12ca4038424763cafda93152794f666300
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-06 22:41:42 -0400

    Added Orders page with submitted orders

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Layout from './containers/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -12,8 +13,9 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Layout>
-            <Route path="/" exact component={BurgerBuilder} />
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Layout>
         </div>
       </BrowserRouter>
