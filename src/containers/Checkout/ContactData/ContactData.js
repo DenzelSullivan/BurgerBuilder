6c26879dc2cb1349af8b18f689ac5ed5bece393c
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-07 23:17:50 -0400

    Added basic form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }, 
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipcode: {
                 elementType: 'input',
@@ -31,7 +39,13 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Zip Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -39,7 +53,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -47,7 +65,11 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Your Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -86,12 +108,30 @@ class ContactData extends Component {
             });
     }
 
+    isValid(value, rules) {
+        let isValid = true;
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength){
+            isValid = value.minLength >= rules.minLength && isValid;
+        }
+        
+        if (rules.maxLength){
+            isValid = value.maxLength <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         // copy of outer form object. name, email, ect...
         const updatedOrderForm = { ...this.state.orderForm }
         // copy of inner form object. type, config, value
         const updatedFormEle = { ...updatedOrderForm[inputIdentifier] };
         updatedFormEle.value = event.target.value;
+        updatedFormEle.valid = this.isValid(updatedFormEle.value, updatedFormEle.validation)
         updatedOrderForm[inputIdentifier] = updatedFormEle;
         this.setState({orderForm: updatedOrderForm})
     }
