4ebe302b2ca067f672dcca60417aefb71c1ea64d
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-05-07 22:36:23 -0400

    Updated form. submit now sends data to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,61 +4,123 @@ import Button from '../../../components/UI/Button/Button';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import styles from './ContactData.module.css';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        }
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest'},
+                        { value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },
+        loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({ loading: true })
+        const formData = {};
+        for (let formEleId in this.state.orderForm){
+            formData[formEleId] = this.state.orderForm[formEleId].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price, // in prod calc on the server side
-            customer: {
-                name: 'Denzel Sullivan',
-                address: {
-                    street: '123 myhouse road',
-                    zipcode: '12345',
-                    country: 'USA'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price ,// in prod calc on the server side
+            orderData: formData
         }
         axios.post('/orders.json', order)
             .then(response => {
-                this.setState({ loading: false});
+                this.setState({ loading: false });
                 this.props.history.push('/');
             })
             .catch(error => {
                 console.log(error)
-                this.setState({ loading: false});
+                this.setState({ loading: false });
             });
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        // copy of outer form object. name, email, ect...
+        const updatedOrderForm = { ...this.state.orderForm }
+        // copy of inner form object. type, config, value
+        const updatedFormEle = { ...updatedOrderForm[inputIdentifier] };
+        updatedFormEle.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormEle;
+        this.setState({orderForm: updatedOrderForm})
+    }
+
     render() {
+        const formEleArray = [];
+        for (let key in this.state.orderForm) {
+            formEleArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
-            <form>
-                    <input className={styles.Input} type="text" name="name" placeholder="Your Name"/>
-                    <input className={styles.Input} type="email" name="email" placeholder="Your Email"/>
-                    <input className={styles.Input} type="text" name="street" placeholder="Street"/>
-                    <input className={styles.Input} type="text" name="postal" placeholder="Postal Code"/>
-                    <Button 
-                        btnType="Success"
-                        clicked={this.orderHandler}>ORDER</Button>
-                </form>
+            <form onSubmit={this.orderHandler} >
+                {formEleArray.map(formEle => (
+                    <Input 
+                        key={formEle.id}
+                        elementType={formEle.config.elementType} 
+                        elementConfig={formEle.config.elementConfig}
+                        value={formEle.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formEle.id)} />
+                ))}
+                <Button btnType="Success">ORDER</Button>
+            </form>
         );
-        if (this.state.loading){
+        if (this.state.loading) {
             form = <Spinner />
         }
-        return(
+        return (
             <div className={styles.ContactData}>
                 <h4>Enter your Contact Data</h4>
                 {form}
